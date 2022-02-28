9df94ed0d0b38f0c6034db97059c8337d511d4db
Alvaro Ramas Franco <alvaroramasfranco@gmail.com>
2022-02-28 23:03:04 +0100

    Cambios en el login hechos

--- lib/loggin_button.dart
+++ lib/loggin_button.dart
@@ -1,47 +1,71 @@
 import 'package:flutter/material.dart';
-
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title, @required this.color
-  }) : super(key: key);
-
-  final String title;
-  final Color? color;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
- 
-  final scaffoldKey = GlobalKey<ScaffoldState>();
+//Clase LoginButton que hereda de la clase StatelessWidget, 
+//abarcará toda la interfaz del login. StatelessWidget, es
+//un widget que no cambia, en este caso podemos utilizar dicha
+//clase, si por ejemplo fuera un contador utilizariamos
+//StatefulWidget, ya que son valores que cambian dinámicamente.
+class LoginButton extends StatelessWidget {
+  LoginButton({Key? key, @required this.onPressed}) : super(key: key);
+   
+ //Variable final que recogerá la clave del formulario, para toda la
+ //aplicación
   final formKey = GlobalKey<FormState>();
+  //Variable de tipo String que permitirá validar el campo email
+  //del formulario.
+  static String? email;
+  //Variable pass que permitirá validar el campo password del formulario.
+  static String? pass;
+  //Boton que validará el formulario.
+  final GestureTapCallback? onPressed;
 
   @override
   Widget build(BuildContext context) {
+    //Retornará un Scaffold, que constituirá toda la estructura del diseño
+    //de la interfaz.
     return Scaffold(
-      key: scaffoldKey,
+      //Barra superior de la aplicación.
       appBar: AppBar(
+        //Propiedad title que almacenará una variable de tipo Text, y se
+        //le pasará un string para indicar el titulo de la aplicación.
         title: const Text("Validando login"),
       ),
+      //Cuerpo de la interfaz, a la cual se le pasa un padding.
       body: Padding(
+        //Propiedad padding, se le indicará un padding para todo de 20 px, 
+        //el padding se aplica con la clase EdgeInsets.
         padding: const EdgeInsets.all(20.0),
+        //Hijo del cuerpo, el cual será un formulario.
         child: Form(
+          //Clave para el formulario que lo hará único.
           key: formKey,
+          //Hijo, que será del tipo SingleChildScrollView, este widget
+          //permitirá que el tamaño de la aplicación se adapte en función
+          //de la interactividad de los usuarios con el formulario.
           child: SingleChildScrollView(
+            //Hijo del widget SingleChildScrollView, será una columna, 
+            //que contendrá varios hijos en un array de Widget.
             child: Column(children: <Widget>[
+              //Se inserta una imagen
               Image.asset('assets/images/login_image.png',
                     width: 440, height: 280),
+              //Se asigna un padding superior de 20px.
               const Padding(padding: EdgeInsets.only(top: 20)),
+              //Se llaman a los widgets del formulario.
               _emailInput(),
               _passwordInput(),
+              //Se hace otro padding, en este caso simétrico de 10 px.
               const Padding(padding: EdgeInsets.symmetric(vertical: 10.0),
               ),
+              //Se usa el widget MaterialButton, indicando un botón.
               MaterialButton(
-                color: widget.color,
-                child: Text(widget.title),
-                onPressed: _boton,
-              ),
+                //Propiedad color, a la que se le asigna el color blanco.
+                color: Colors.white,
+                //Propiedad child, que tendrá un Text, con una cadena para
+                //el botón.
+                child: const Text('Entrar'),
+                //Se utiliza la función onPressed que hemos creado y se le pasa
+                //a la propiedad onPressed.
+                onPressed: onPressed),
             ],
             ),
         ),
@@ -50,62 +74,36 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-   mostrarResultado(){
-    return const AlertDialog(
-      title: Text("Login realizado"),
-    );
-  }
-
-
+//Método que devuelve un Padding, con el TextField del campo email.
   Widget _emailInput() {
+    //Devolverá un padding.
     return Padding(
+      //Se le asigna un padding para todo de 16 px.
         padding: const EdgeInsets.all(16.0),
+        //Tiene un nodo hijo, de tipo TextFormField, indicando
+        //que se trata de un campo de tipo formulario.
         child: TextFormField(
-            decoration:
-                const InputDecoration(hintText: 'Email', label: Text('Email')),
-            validator: (value) => _validarEmail(value!)));
+          //Método que permitirá verificará el cambio de valor del formulario.
+          onChanged: ((value) => email = value),
+          //propiedad decoration, a la que se le indicará decoración con la clase
+          //InputDecoration, se le indica la propiedad hintText y se le pasará
+          //una indicación de que se trata de Email, y se le indica
+          //la propiedad label para asignar una etiqueta que es Email.
+            decoration: const InputDecoration(hintText: 'Email', label: Text('Email')),
+            ));
   }
-
-  _boton() {
-    //Cuando se presione el botón
-    final form = formKey.currentState;
-    if (form!.validate()) {
-      form.save();
-    }
-  }
-
-  
-
+//Método que devuelve un Padding, con el TextField del campo password.
   Widget _passwordInput() {
+    //Lo mismo pero orientado al campo contraseña.
     return Padding(
         padding: const EdgeInsets.all(16.0),
         child: TextFormField(
+          onChanged: ((value) => pass = value),
           decoration: const InputDecoration(
               hintText: 'Password', label: Text('Password')),
-          validator: (value) => _validarPassword(value!),
+          //Propiedad obscureText, con la que podremos indicar que se va a recibir una
+          //contraseña por lo tanto cada caracter será sustituido por asteriscos.
           obscureText: true,
         ));
   }
-
-  String _validarEmail(String value) {
-  String correo = "usuario@gmail.com";
-
-  if (correo != value) {
-    return 'El correo no es correcto';
-  } else {
-    return 'El correo es correcto';
-  }
 }
-
-String _validarPassword(String value) {
-  String password = "usuario";
-
-  if (password != value) {
-    return 'La contraseña no es correcta';
-  } else {
-    return 'La contraseña es correcta';
-  }
-}
-}
-
-
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,89 @@
 import 'package:flutter/material.dart';
 import 'loggin_button.dart';
 
+// Método main de la aplicación
 void main() => runApp(const MyApp());
 
+// Clase a lanzar por el método main, se encarga de la barra superior con el título
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
 
+// Método que construye un Widget y devuelve el diseño que se le indique.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Componente login',
         theme: ThemeData(),
-        home: const MyHomePage(color: Colors.white, 
-        title: 'Entrar'),
+        home: const Estado(),
         );
   }
+}
 
+//Se crea un clase Estado que hereda de la clase
+//StatefulWidget, que permitirá crear el estado de la aplicación.
+class Estado extends StatefulWidget {
+  const Estado({Key? key}) : super(key: key);
+
+  @override
+  State <Estado> createState() => _Estado();
+}
+//Clase _Estado que extiende de la clase Estado, la clase _Estado
+//implementará el método sobreescrito build, para construir en el
+//contexto el botón de validación.
+class _Estado extends State<Estado>{
+  @override
+  Widget build(BuildContext context) {
+    //Objeto de la clase LoginButton
+    //Retornará el botón con el método aplicado tomando los campos email y password.
+    return LoginButton(onPressed: (() => _validarLogin(LoginButton.email, LoginButton.pass)),
+    );
+  }
+
+
+//Método que permitirá validar el correo y la contraseña
+//que se indiquen en el formulario.
+  _validarLogin(String? email, String? pass) {
+  String correo = "usuario@gmail.com";
+  String password = "usuario";
+
+  if(email != correo && pass != password){
+    return _mensajeInformativo('Error', 'Los datos son incorrectos');
+  }
+  else {
+    return _mensajeInformativo('Login completado', 'Credenciales correctas');
+
+  }
+}
+
+//Método que retornará una alerta de dialogo, tomando
+//en cuenta las comprobación del método _validarLogin.
+ _mensajeInformativo(String titulo, String mensaje) {
+    return showDialog(
+      //contexto de compilación de la aplicación.
+      context: context,
+      //contruye un contexto que será utilizado en la alerta
+      //de dialogo
+      builder: (BuildContext context) {
+        return AlertDialog(
+          //propiedad title, que recibirá un string.
+          title: Text(titulo),
+          //propiedad content, que indica el contenido, de tipo String.
+          content: Text(mensaje),
+          //acciones, que será una lista de componentes,
+          //en este caso será el componente TextButton.
+          actions: <Widget>[
+            TextButton(
+              //Este componente tendrá un hijo con el texto de dicho botón, 
+              //y estilo del texto.
+              child: Text("CERRAR", style: TextStyle(color: Colors.black),),
+              //Función onPressed del componente TextButton, cuando se haga
+              //clic en el botón de Cerrar, se cerrará la alerta de dialogo.
+              onPressed: (){ Navigator.of(context).pop(); },
+            )
+          ],
+        );
+      });
+ }
+  
 }
