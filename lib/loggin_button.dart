e9de333966706ad563187da9289f510acbbedb43
Alvaro Ramas Franco <alvaroramasfranco@gmail.com>
2022-03-02 19:03:06 +0100

    Quitando foto

--- lib/loggin_button.dart
+++ lib/loggin_button.dart
@@ -46,8 +46,8 @@ class LoginButton extends StatelessWidget {
             //que contendrá varios hijos en un array de Widget.
             child: Column(children: <Widget>[
               //Se inserta una imagen
-              Image.asset('assets/images/login_image.png',
-                    width: 440, height: 280),
+              // Image.asset('assets/images/login_image.png',
+              //       width: 440, height: 280),
               //Se asigna un padding superior de 20px.
               const Padding(padding: EdgeInsets.only(top: 20)),
               //Se llaman a los widgets del formulario.
